3c51f1f267d516d0e7189b6099fbf601135707d5
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2022-03-30 22:34:02 +0600

    Add About basic markup and styles

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,9 +1,69 @@
 import React from "react";
+import { FaHtml5, FaCss3Alt, FaSass, FaReact, FaDocker } from "react-icons/fa";
+import { SiJavascript } from "react-icons/si";
+import Porfolio1 from "../../images/porfolio-1.png";
+import Porfolio2 from "../../images/porfolio-2.jpg";
+import Porfolio3 from "../../images/porfolio-3.png";
+
+import "./about.scss";
 
 function About() {
   return (
-    <section className="home">
-      <h1>About Page</h1>
+    <section className="about">
+      <div className="container">
+        <div className="about__intro">
+          <h1>Hello am Hero Coder!</h1>
+          <p>Frontend React Developer</p>
+        </div>
+
+        <div className="about__skills">
+          <h3>My Skills</h3>
+          <ul>
+            <li>
+              <FaHtml5 />
+              HTML
+            </li>
+            <li>
+              <FaCss3Alt />
+              CSS
+            </li>
+            <li>
+              <SiJavascript />
+              JavaScript
+            </li>
+            <li>
+              <FaSass />
+              Sass
+            </li>
+            <li>
+              <FaReact />
+              React
+            </li>
+            <li>
+              <FaDocker />
+              Docker
+            </li>
+          </ul>
+        </div>
+
+        <div className="about__portfolio">
+          <h3>Latest Works</h3>
+          <ul>
+            <li>
+              <img src={Porfolio1} alt="" />
+              <h4>Dashboard for Cargo</h4>
+            </li>
+            <li>
+              <img src={Porfolio2} alt="" />
+              <h4>Marketplace App</h4>
+            </li>
+            <li>
+              <img src={Porfolio3} alt="" />
+              <h4>Landing Page</h4>
+            </li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 }
