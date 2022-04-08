a6a894f9d5c324c82600fac05acdc777a580c7a7
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2022-04-08 22:33:25 +0600

    add Contact and Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
-import { Navbar } from "./components";
+import { Navbar, Footer } from "./components";
 import { Home, About, Blog, Contact, Notfound } from "./Pages";
 
 import "./app.scss";
@@ -21,6 +21,10 @@ function App() {
           <Route path="*" element={<Notfound />} />
         </Routes>
       </main>
+
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,10 +1,99 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import marker from "../../images/marker.svg";
+import L from "leaflet";
+
+import "./contact.scss";
 
 function Contact() {
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+    reset,
+  } = useForm({
+    mode: "onBlur",
+  });
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+    reset();
+  };
+
+  const position = [42.87789474013635, 74.58761239970043];
+
+  const icon = L.icon({
+    iconUrl: marker,
+    iconAnchor: position,
+    iconSize: [55, 55],
+  });
+
   return (
     <section className="contact">
-      adsfasd1213.asasdf234@asdfasdf.com 00 996 312 55 55 55
-      <h1>Contact Page</h1>
+      <h1>Our Contacts</h1>
+      <div className="container contact__container">
+        <div className="contact__col">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <label>
+              Your Name
+              <input
+                placeholder="John Doe"
+                {...register("name", {
+                  required: "Имя должно быть заполнено!",
+                  minLength: {
+                    value: 3,
+                    message: "Минимум 3 символа!",
+                  },
+                })}
+              />
+            </label>
+            <div className="contact__error">
+              {errors?.name && <p>{errors?.name?.message || "Error!"}</p>}
+            </div>
+            <label>
+              Your Email
+              <input
+                placeholder="john@company.com"
+                {...register("email", {
+                  required: "Email должно быть заполнено!",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i,
+                    message: "Введите корректный email!",
+                  },
+                })}
+              />
+            </label>
+            <div className="contact__error">
+              {errors?.email && <p>{errors?.email?.message || "Error!"}</p>}
+            </div>
+            <label>
+              Your Message
+              <textarea
+                {...register("msg", {
+                  maxLength: {
+                    value: 10,
+                    message: "Максимум 10 символов",
+                  },
+                })}
+              ></textarea>
+            </label>
+            <div className="contact__error">
+              {errors?.msg && <p>{errors?.msg?.message || "Error!"}</p>}
+            </div>
+            <input type="submit" disabled={!isValid} />
+          </form>
+        </div>
+
+        <div className="contact__col">
+          <MapContainer center={position} zoom={25}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={position} icon={icon}>
+              <Popup>Филармония</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
     </section>
   );
 }
